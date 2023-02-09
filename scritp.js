555105d39ba2226c751e820a18ed22f072820075
sevaK5 <43607822+sevaK5@users.noreply.github.com>
2023-02-09 21:43:27 +0700

    add delete function, add new task when press enter and validate task text for empty string

--- scritp.js
+++ scritp.js
@@ -1,8 +1,12 @@
+
+
+
+
 function addTask() {
 
     const inputField = document.querySelector('#input-field');
     let taskText = inputField.value;
-    if (taskText === '') return;
+    if (taskText === '' || taskText.match(/^ +$/g)) return;
     console.log(taskText);
     const tasksContainer = document.querySelector('#tasks-container');
     const divTask = document.createElement('div');
@@ -14,12 +18,38 @@ function addTask() {
     const crossBtn = document.createElement('button');
     crossBtn.textContent = '✖';
     crossBtn.setAttribute('onclick', 'deleteTask()');
+    crossBtn.className = 'remove-task'
     divTask.append(inputCheckbox, innerTaskText, crossBtn);
     tasksContainer.appendChild(divTask);
     inputField.value = '';
 }
 
+
+let inputField = document.querySelector('#input-field');
+
+
+inputField.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.querySelector('button').click();
+    }
+});
+
+
 function deleteTask() {
+
+
+
+    
+    for (let element of event.composedPath()) {
+        if (element.matches && element.matches("button.remove-task")) {
+
+            console.log(element, 'is the button clicked');
+            console.log(element.parentNode, 'is the li element');
+            element.parentNode.remove();
+
+        }
+    }
     
     
 }
